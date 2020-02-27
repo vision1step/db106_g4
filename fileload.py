da3b627bbf9cc44569349d793d5aac05e74d364a
htlingune <59502548+htlingune@users.noreply.github.com>
2020-02-27 12:13:12 +0800

    Update fileload.py

--- fileload.py
+++ fileload.py
@@ -1,4 +1,9 @@
 import json
+import pandas as pd
+
 with open(r"filepath",'r',encoding= 'utf-8') as load_f:
     load_dict = json.load(load_f)
     print(load_dict)
+
+df = pd.read_json(r"filepath")
+
