355f7fe2b1ae9c26031a734da286197edc2374b4
htlingune <59502548+htlingune@users.noreply.github.com>
2020-03-06 20:38:21 +0800

    Update fileload.py

--- fileload.py
+++ fileload.py
@@ -1,9 +1,14 @@
 import json
-import pandas as pd
+#import pandas as pd
 
 with open(r"filepath",'r',encoding= 'utf-8') as load_f:
     load_dict = json.load(load_f)
-    print(load_dict)
+    content = load_dict['content']
 
-df = pd.read_json(r"filepath")
+#df = pd.read_json(r"filepath")
 
+import pymongo
+import dns
+client = pymongo.MongoClient("mongodb+srv://db_106_user:stockdb106@cluster0-iocac.gcp.mongodb.net/test?retryWrites=true&w=majority")
+db = client.newsjson
+db.inventory.insert_one(load_dict)
